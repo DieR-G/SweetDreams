db5e70c386b487ca33fd38456c1931e5585bd4c0
DieR-G <00043920@uca.edu.sv>
2021-11-19 15:09:47 -0600

    fix colors bug

--- final-project/tailwind.config.js
+++ final-project/tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      "lightgreen": "#6CC471",
-    },
     extend: {
+      colors: {
+        "lightgreen": "#6CC471",
+      },
       fontFamily: {
         normal: ['Varela'],
         round: ["Varela Round"],
