d65d3a8d423dfab277a3aa9bf7436e30fc0dcca7
Eddy Barahona <barahona.hernandez2712@gmail.com>
2021-11-20 17:01:30 -0600

    admin view's design finished

--- final-project/src/Components/Admin/CreateEditForm/CreateEditForm.jsx
+++ final-project/src/Components/Admin/CreateEditForm/CreateEditForm.jsx
@@ -3,41 +3,52 @@ import React from 'react';
 const CreateEditForm = () => {
     return (
         <div className='bg-purple-900 w-1/2 flex flex-col items-center'>
-            <form action='' className='flex flex-col mt-5'>
-                <h2 className='text-center'>Create Post</h2>
+            <form action='' className='flex flex-col w-4/5 mt-5 bg-gray-100 p-5 rounded-lg'>
+                <h2 className='text-center text-3xl font-normal font-bold'>Create post</h2>
 
-                <label for='title-input' className='mt-2'>Title</label>
+                <label htmlFor='title-input' className='mt-2'>Title</label>
                 <input 
                     type='text' 
                     id='title-input' 
                     name='title' 
                     placeholder='f.e. The benefits of exercise' 
-                    className='mt-1' 
+                    className='mt-1 border-2 border-gray-300 rounded px-3 py-1' 
                     required
                 />
 
-                <label for='description-text-area' className='mt-4'>Description</label>
+                <label htmlFor='description-text-area' className='mt-4'>Description</label>
                 <textarea 
                     id='description-text-area' 
                     name='description' 
                     cols='40' 
                     rows='5' 
                     placeholder='f.e. The health benefits of regular physical activity and exercise cannot be ignored. Everyone benefits from exercise, regardless of age, gender, or physical ability.' 
-                    className='mt-1'
+                    className='mt-1 border-2 border-gray-300 rounded px-3 py-1'
                     required 
                 />
 
-                <label for='image-input' className='mt-4'>Image</label>
+                <label htmlFor='image-input' className='mt-4'>Image</label>
                 <input 
                     type='text' 
                     id='image-input' 
                     name='image' 
                     placeholder='f.e. https://health.clevelandclinic.org/wp-content/uploads/sites/3/2013/09/inexpensiveExercise-1277759983-770x533-1.jpg' 
-                    className='mt-1'
+                    className='mt-1 border-2 border-gray-300 rounded px-3 py-1'
                     required 
                 />
 
-                <button type='submit' id='submit' className='mt-5'>Create post</button>
+                <div className='flex justify-evenly w-full'>
+                    <button 
+                        id='clear-all' 
+                        className='mt-5 rounded px-2 py-1 w-2/6 text-center bg-gray-300 hover:bg-gray-500 self-center'
+                    >Clear all</button>
+
+                    <button 
+                        type='submit' 
+                        id='submit' 
+                        className='mt-5 rounded px-2 py-1 w-2/6 text-center text-white bg-pink-500 hover:bg-pink-700 self-center'
+                    >Create post</button>
+                </div>
             </form>
         </div>
     );
--- final-project/src/Components/Admin/ProductViewer/Product/Product.jsx
+++ final-project/src/Components/Admin/ProductViewer/Product/Product.jsx
@@ -8,19 +8,19 @@ const Product = () => {
                 <h2 className='font-round text-base'> Title: Programación web </h2>
             </div>
 
-            <div className='flex justify-between w-3/12 h-full text-white text-3xl'>
+            <div className='flex justify-between w-3/12 h-full text-white text-2xl'>
                 <div onClick={ ()=> {} }
                     className='flex justify-center items-center bg-green-600 hover:bg-green-700 w-4/12 h-full cursor-pointer'>
                     <AiOutlineEye />
                 </div>
 
                 <div onClick={ ()=> {} }
-                    className='flex justify-center items-center bg-yellow-500 hover:bg-yellow-700 w-4/12 h-full items-center cursor-pointer'>
+                    className='flex justify-center items-center bg-yellow-500 hover:bg-yellow-700 w-4/12 h-full cursor-pointer'>
                     <BiEditAlt />
                 </div>
 
                 <div onClick={ ()=> {} }
-                    className='flex justify-center items-center bg-red-500 hover:bg-red-800 w-4/12 h-full items-center cursor-pointer'>
+                    className='flex justify-center items-center bg-red-500 hover:bg-red-800 w-4/12 h-full cursor-pointer'>
                     <FiTrash2 />
                 </div>
             </div>
