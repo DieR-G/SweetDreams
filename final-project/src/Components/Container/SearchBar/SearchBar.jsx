e4bd5310e4923d10cac8aeea318ffd252796e6b9
Victor Daniel Peraza Bolaños <00143320@uca.edu.sv>
2021-11-28 23:43:01 -0600

    last touches

--- final-project/src/Components/Container/SearchBar/SearchBar.jsx
+++ final-project/src/Components/Container/SearchBar/SearchBar.jsx
@@ -5,9 +5,9 @@ import SearchIcon from '../../../Assets/Search.svg'
 import SessionContext from '../../../Contexts/SessionContext';
 
 
-const SearchBar = ({ searchFunction = () => { }, clearFunction = () => { }, loading = ()=>{} }) => {
+const SearchBar = ({ searchFunction = () => { }, clearFunction = () => { }, loading = () => { } }) => {
     const [search, SetSearch] = useState("");
-    const {authenticated} = useContext(SessionContext);
+    const { authenticated } = useContext(SessionContext);
 
     useEffect(() => {
         console.log(search);
@@ -44,7 +44,7 @@ const SearchBar = ({ searchFunction = () => { }, clearFunction = () => { }, load
 
                     if (response['data'].some(post => post.title === searchText)) {
                         found = true;
-                       
+
                     }
                     else
                         page += 1;
@@ -57,7 +57,7 @@ const SearchBar = ({ searchFunction = () => { }, clearFunction = () => { }, load
 
             if (found) {   //retriving the post needed 
                 const searchedPost = response['data'].filter(post => post.title === searchText);
-         
+
                 setter(searchedPost)
 
             }
@@ -78,7 +78,7 @@ const SearchBar = ({ searchFunction = () => { }, clearFunction = () => { }, load
 
     return (
         <div className="flex  flex-col flex-wrap justify-center justify-items-center content-evenly ">
-            <div className="w-1/2  h-18 bg-purple-500  p-4 rounded-xl shadow-lg   ">
+            <div className="w-1/2  h-18 bg-loginpattern  p-4 rounded-xl shadow-lg   ">
                 <form action="" className="w-auto h-auto flex flex-row flex-wrap justify-center justify-items-center content-evenly">
                     <input type="text" className="w-2/3 h-12 bg-purple-300  rounded-xl mr-2  my-2 font-normal p-4 text-center  u-sm:h-12" onChange={(e) => { SetSearch(e.target.value) }} value={search} />
                     <Button localStyle="w-12 h-12  ml-4 my-2" background={SearchIcon} onClick={(e) => { e.preventDefault(); searchPost(search, searchFunction); clearFunction(true); SetSearch("") }} />
